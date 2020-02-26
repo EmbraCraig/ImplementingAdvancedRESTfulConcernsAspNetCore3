16ffafcabc545ce8cb6635d88ba2be7b8ff47962
Craig McCallum <Craig.McCallum@zonal.co.uk>
2020-02-26 17:00:23 +0000

    Course section 2 - implement paging on Authors endpoints

--- Starter files/CourseLibrary/CourseLibrary.API/Services/CourseLibraryRepository.cs
+++ Starter files/CourseLibrary/CourseLibrary.API/Services/CourseLibraryRepository.cs
@@ -4,6 +4,7 @@ using CourseLibrary.API.ResourceParameters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using CourseLibrary.API.Helpers;
 
 namespace CourseLibrary.API.Services
 {
@@ -123,19 +124,13 @@ namespace CourseLibrary.API.Services
             return _context.Authors.ToList<Author>();
         }
 
-        public IEnumerable<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters)
+        public PagedList<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters)
         {
             if (authorsResourceParameters == null)
             {
                 throw new ArgumentNullException(nameof(authorsResourceParameters));
             }
-
-            if (string.IsNullOrWhiteSpace(authorsResourceParameters.MainCategory)
-                 && string.IsNullOrWhiteSpace(authorsResourceParameters.SearchQuery))
-            {
-                return GetAuthors();
-            }
-
+            
             var collection = _context.Authors as IQueryable<Author>;
 
             if (!string.IsNullOrWhiteSpace(authorsResourceParameters.MainCategory))
@@ -152,8 +147,10 @@ namespace CourseLibrary.API.Services
                     || a.FirstName.Contains(searchQuery)
                     || a.LastName.Contains(searchQuery));
             }
-
-            return collection.ToList();
+            
+            return PagedList<Author>.Create(collection, 
+                authorsResourceParameters.PageNumber, 
+                authorsResourceParameters.PageSize);
         }
 
         public IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds)
